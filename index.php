ee1aef500a273d0a8e140b95ae89dbc213a2bb02
Thomas van Otterloo <movecraft.pirates@gmail.com>
2019-10-18 11:58:39 +0200

    Basic Home Page

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>BRO | Home</title>
+	</head>
+	<body>
+		<!--Title Balk-->
+		 <div id="title_panel">
+		 	<h1>Home</h1>
+		 	<li> <a href="index.html" id="home_button"></a></li>
+		 </div>
+		 <!--Navigatie Balk-->
+		 <div id="menu_panel">
+		 	<li> <a href="" id="btn kleding_button">Kleding</a></li>
+		 	<li> <a href="" id="btn school_button">School</a></li>
+		 	<li> <a href="" id="btn werk_geld_button">Werk En Geld</a></li>
+		 	<li> <a href="" id="btn games_button">Games</a></li>
+		 </div>
+	</body>
+</html>
